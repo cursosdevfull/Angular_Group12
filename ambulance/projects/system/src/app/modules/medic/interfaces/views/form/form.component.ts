bcc797db9f3de5c7356314f401d278ec817a0a4d
Sergio Hidalgo <shidalgoc@auna.pe>
2022-11-20 10:10:19 -0500

    providers

--- ambulance/projects/system/src/app/modules/medic/interfaces/views/form/form.component.ts
+++ ambulance/projects/system/src/app/modules/medic/interfaces/views/form/form.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+
+import { MedicApplication } from '../../../application/medic.application';
+import { MedicRepository } from '../../../domain/repositories/medic.repository';
+import { MedicInfrastructure } from '../../../infrastructure/medic.infrastructure';
 
 @Component({
   selector: 'amb-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, OnChanges {
+  constructor() {}
 
-  constructor() { }
+  ngOnChanges() {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    const infrastructure: MedicRepository = new MedicInfrastructure();
+    const application = new MedicApplication(infrastructure);
+
+    await application.createMedic(
+      'Juan',
+      'Carrasco',
+      '134',
+      '999999999',
+      'email@correo.com'
+    );
   }
-
 }
